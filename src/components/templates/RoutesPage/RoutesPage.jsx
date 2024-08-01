6ca2aea010b6d1120ec6b259f988d5b0abe7d865
Vanessa Lima <vanessalima951@gmail.com>
2024-08-01 10:50:24 -0300

    fix: inclusão de polylines na tela de rotas

--- src/components/templates/RoutesPage/RoutesPage.jsx
+++ src/components/templates/RoutesPage/RoutesPage.jsx
@@ -5,14 +5,17 @@ import List from "@/components/organisms/List/List";
 import { Container } from "@/styles/generics/Container.styles";
 import { route } from "@/utils/route";
 import MapWrapper from "@/components/organisms/MapWrapper/MapWrapper";
+import { MapWrapperContext } from "@/context/MapWrapperContext";
+import { useContext } from "react";
 
 export default function RoutesPage() {
+  const { polylines } = useContext(MapWrapperContext);
   return (
     <S.RoutesPage>
       <PageTitle text="Rotas" />
       <PageSubtitle text="Visualize aqui o mapa com as rotas dos pedidos. " />
       <Container id="container">
-        <MapWrapper />
+        <MapWrapper polylines={polylines}/>
         <List listType="routes" items={route} />
       </Container>
     </S.RoutesPage>
